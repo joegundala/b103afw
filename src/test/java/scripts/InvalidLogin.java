e269f8fc4a50c6dd7e80f248e4cb0d16a1155ce0
joegundala <joegundala89@gmail.com>
2024-07-10 08:10:06 +0530

    data driven test updated invalid login

--- src/test/java/scripts/InvalidLogin.java
+++ src/test/java/scripts/InvalidLogin.java
@@ -4,19 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Utility;
 import page.LoginPage;
 
 public class InvalidLogin extends BaseTest
 {
 	@Test(priority = 2)
-	public void testInvalidLogin()
-	{
+	public void testInvalidLogin()	{
+		
+		String un = Utility.getExcelData(EXCEL_PATH, "InvalidLogin",1, 0);
+		String pw = Utility.getExcelData(EXCEL_PATH, "InvalidLogin",1, 1);
 //		1. enter invalid user name
 		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName("abc");
+		loginPage.setUserName(un);
 		
 //		2. enter invalid password
-		loginPage.setPassword("xyz");
+		loginPage.setPassword(pw);
 		
 //		3. click on go button
 		loginPage.clickGoButton();
